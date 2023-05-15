f1782dfa76a258536d7dec0b815015476830577b
Justin-Sunny01 <117711435+Justin-Sunny01@users.noreply.github.com>
2023-05-15 18:38:01 +0530

    removed hashtag

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:url_strategy/url_strategy.dart';
 
 import 'routes/routes_import.dart';
 
 void main() {
+  // this plugin method remove hashtag from url --[setPathUrlStrategy]--
+  setPathUrlStrategy();
   runApp(MyApp());
 }
 
@@ -10,12 +13,10 @@ class MyApp extends StatelessWidget {
   MyApp({super.key});
 
   final _appRouter = AppRouter();
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
-      title: 'Flutter Demo',
+      title: 'Flutter Auto Route',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
